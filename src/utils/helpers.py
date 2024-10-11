1d20537defa5cbd4867f72f0da08d67686ead76f
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-11 17:15:01 +0000

    Refactor shift_date_by_window function in helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -21,27 +21,6 @@ def get_class_methods_exclude_dunder(class_object: object) -> list:
             if callable(getattr(class_object, func)) 
             and not func.startswith("__")]
 
-def shift_date_by_window(
-    date_str: str,
-    window: int,
-    direction: str,
-    date_frequency: str
-) -> str:
-    """Shifts the date by a window in the specified direction."""
-    try:
-        date_freq_number = re.search(r"\d+", date_frequency)[0]
-    except TypeError:
-        date_freq_number = 1
-
-    date_freq_units = re.search(r"(\D+)$", date_frequency)[0]
-    
-    if direction not in ["forward", "backward"]:
-        raise ValueError("Direction must be 'forward' or 'backward'")
-    shift_periods = int(date_freq_number) * window
-    shift_periods = pd.Timedelta(shift_periods, unit=date_freq_units) # type: ignore
-    shifted_date = pd.to_datetime(date_str) + shift_periods if direction == 'forward' else pd.to_datetime(date_str) - shift_periods
-    return shifted_date.strftime("%Y-%m-%d %H:%M:%S")
-
 def get_classes_by_string_name(string_name: str, module_names: list[str]) -> dict[str, Any]:
     """Gets the class associated to a string name."""
     classes = {}
@@ -56,3 +35,4 @@ def get_classes_by_string_name(string_name: str, module_names: list[str]) -> dic
                 classes[name] = obj
 
     return classes
+
