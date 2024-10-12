79fab3152a230f588d413e8735cd2d98f72a7810
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-12 09:43:48 +0000

    Refactor preprocessing.py to use DatabaseDataManager for loading and saving data

--- src/loaders/preprocessing.py
+++ src/loaders/preprocessing.py
@@ -1,66 +1,21 @@
-import math
-import logging
+
 import pandas as pd
-from abc import ABC, abstractmethod
+
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from typing import Any, Hashable, Optional
 
 from sqlalchemy import (
     select,
-    join,
     or_,
-    func,
     and_,
     PrimaryKeyConstraint,
-    distinct,
-    delete,
-    sql
 )
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.engine import Connection
-from sqlalchemy.orm import sessionmaker
 
+from loaders.base import DatabaseDataManager
 from database.models.preprocessing import EngineeredFeaturesTable
 from database.schemas.preprocessing import EngineeredFeaturesSchema
-
-@dataclass
-class DatabaseDataManager(ABC):
-    """Abstract class to load the data."""
-    connection: Connection
-    logger: logging.Logger
-    
-    def __post_init__(self) -> None:
-        self.Session = sessionmaker(bind=self.connection)
-
-    def load(self) -> None:
-        """Abstract function used to load the data from a database."""
-
-    def save(self, df: pd.DataFrame, batch_size: int) -> None:
-        """
-        Save the data to the database table.
-        """
-        df_dict = df.to_dict(orient='records')
-        df_dict = [{k: sql.null() if isinstance(v, float) and math.isnan(v) else v for k, v in row.items()} for row in df_dict]
-
-        size = len(df_dict)
-        self.logger.debug(f"Inserting {size} rows into the database...")
-
-        if size > batch_size:
-            n_batches = size // batch_size
-
-            for i in range(n_batches):
-                start = i * batch_size
-                end = (i + 1) * batch_size - 1
-                self.logger.debug(f"Inserting data from dates {df_dict[start]['timestamp']} to {df_dict[end]['timestamp']}...")
-                self._statement_insert_to_db(df_dict[start:end])
-            self._statement_insert_to_db(df_dict[n_batches * batch_size:])
-
-        else:
-            self._statement_insert_to_db(df_dict)
-
-    def _statement_insert_to_db(self) -> None:
-        """Abstract method with the statement used to save the data to the database."""
 
 
 @dataclass
@@ -68,7 +23,6 @@ class EngineeredFeaturesManager(DatabaseDataManager):
     """
     Loads the data from the engineered_features table.
     """
-    
     def load(self,
             latitude: str,
             longitude: str,
