0c69af937e424da13ebade1b45a47496a2156329
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-12 19:15:37 +0200

    updated

--- postgres/sql/engineered_features.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS engineered_features (
-	"timestamp" TIMESTAMP WITHOUT TIME ZONE,
-	"latitude" REAL,
-	"longitude" REAL,
-	"variable_code" TEXT,
-	"value" REAL,
-	"update_date" TIMESTAMP WITHOUT TIME ZONE,
-	CONSTRAINT pk_engineered_features PRIMARY KEY ("timestamp", latitude, longitude, variable_code)
-);
