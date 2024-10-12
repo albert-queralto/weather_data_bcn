9e613e957aff69e964e2cd7069d62924534f9f1c
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-12 16:15:23 +0000

    Refactor main.py to improve preprocessing and add date handling

--- src/preprocessing/main.py
+++ src/preprocessing/main.py
@@ -1,7 +1,7 @@
 """
 Example command line:
 ---------
-- python "/root/home/src/preprocessing/main.py" -lat 41.389 -lon 2.159 -sd "2015-10-11" -ed "2023-03-29"
+- python "/root/home/src/preprocessing/main.py" -lat 41.389 -lon 2.159 -sd "2015-10-11" -ed "2024-10-12"
 """
 
 import os
@@ -9,7 +9,8 @@ import sys
 import time
 import pandas as pd
 from dataclasses import dataclass
-from typing import Any
+from datetime import datetime
+from typing import Any, Optional
 from pathlib import Path
 import sqlalchemy
 
@@ -33,11 +34,12 @@ from utils.file_handlers import TomlHandler
 from utils.custom_logger import CustomLogger
 from loaders.api import OpenMeteoDataManager
 
-from utils.helpers import get_class_methods_exclude_dunder
+from utils.helpers import get_class_methods_exclude_dunder, shift_date_by_window
 
 from database.connections import DatabaseConnection, ConnectionStringBuilder
 from utils.seasonal_features import CreateSeasonalFeatures
 from loaders.preprocessing import EngineeredFeaturesManager
+from loaders.utils import LastProcessedDataManager
 
 
 @dataclass
@@ -86,6 +88,83 @@ class Preprocessor:
         df["longitude"] = longitude
         saver.save(df, batch_size)
 
+        last_date_saver = LastProcessedDataManager(self.connection, self.logger)
+        last_date_saver.save(latitude, longitude, "preprocessing", df['timestamp'].max())
+        self.logger.debug(f"Last date processed {df['timestamp'].max()} saved to the database...")
+
+    def create_start_date(self,
+            start_date: Optional[str],
+            start_time_window: int,
+            direction: str,
+            date_frequency: str,
+            last_date_processed: Optional[str]
+        ) -> Optional[str]:
+        if start_date in [None, 'None']:
+            date = datetime.now().strftime("%Y-%m-%d")
+            if last_date_processed:
+                self.logger.debug(f"Last date: {last_date_processed}")
+                date = datetime.strftime(last_date_processed, "%Y-%m-%d")
+            return shift_date_by_window(date, start_time_window, direction, date_frequency)
+        return start_date
+
+    def create_end_date(self,
+            end_date: Optional[str],
+            
+        ) -> tuple[Optional[str], Optional[str]]:
+        if end_date in [None, 'None']:
+            end_date = datetime.now().strftime("%Y-%m-%d")
+        return end_date
+
+def main(
+        connection: sqlalchemy.engine.Connection,
+        latitude: float,
+        longitude: float,
+        start_date: str,
+        end_date: str,
+        preprocessing_config: dict
+    ):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "preprocessing_config": preprocessing_config
+    }
+    logger.debug("Preprocessing the data with the following parameters:")
+    for k, v in params.items():
+        logger.debug(f"{k}: {v}")
+    
+    last_date_loader = LastProcessedDataManager(
+        connection=connection,
+        logger=logger
+    )
+
+    last_date = last_date_loader.load(latitude, longitude, preprocessing_config['data_type'])
+    preprocessor = Preprocessor(logger, connection)
+
+    start_time = preprocessor.create_start_date(
+        start_date=start_date,
+        start_time_window=preprocessing_config['start_time_window'],
+        direction=preprocessing_config['direction'],
+        date_frequency=preprocessing_config['date_frequency'],
+        last_date_processed=last_date
+    )
+    end_time = preprocessor.create_end_date(end_date)
+    logger.debug(f"The start and end dates are: {start_time} | {end_time}")
+
+    logger.debug("Loading the data...")
+    df = preprocessor.load_data(latitude, longitude, start_time, end_time)
+    
+    logger.debug("Filling null values...")
+    null_filled_df = preprocessor.null_values_filling(df)
+    
+    logger.debug("Creating seasonal features...")
+    seasonal_feats_creator, seasonal_methods = preprocessor.create_seasonal_variables()
+    seasonal_df = seasonal_feats_creator(null_filled_df, seasonal_methods)
+
+    logger.debug("Saving the data to the database...")
+    preprocessor.save_data_to_db(seasonal_df, latitude, longitude, DB_BATCH_SIZE)
+
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -94,6 +173,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     CONFIG_DICT = TomlHandler("config.toml").load()
+    preprocessing_config = CONFIG_DICT.get("preprocessing", {})
     LOGGER_CONFIG = TomlHandler("logger.toml").load()
 
     filename = Path(__file__).resolve().stem
@@ -109,16 +189,11 @@ if __name__ == "__main__":
             )
     postgres_connect = DatabaseConnection().connect(postgres_connection_string)
     
-    preprocessor = Preprocessor(logger, postgres_connect)
-    logger.debug("Loading the data...")
-    df = preprocessor.load_data(args.latitude, args.longitude, args.start_date, args.end_date)
-    
-    logger.debug("Filling null values...")
-    null_filled_df = preprocessor.null_values_filling(df)
-    
-    logger.debug("Creating seasonal features...")
-    seasonal_feats_creator, seasonal_methods = preprocessor.create_seasonal_variables()
-    seasonal_df = seasonal_feats_creator(null_filled_df, seasonal_methods)
-
-    logger.debug("Saving the data to the database...")
-    preprocessor.save_data_to_db(seasonal_df, args.latitude, args.longitude, DB_BATCH_SIZE)
+    main(
+        connection=postgres_connect,
+        latitude=args.latitude,
+        longitude=args.longitude,
+        start_date=args.start_date,
+        end_date=args.end_date,
+        preprocessing_config=preprocessing_config
+    )
