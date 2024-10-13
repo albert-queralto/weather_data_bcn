2baf37f5dcd0621ecb6aef88232478887c817e01
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-13 19:35:25 +0200

    Add App component, Navbar component, ChartComponent, and related files

---
+++ src/frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import ChartComponent from './components/Chart';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+            <Navbar />
+            <ChartComponent />
+        </div>
+        <div>
+            
+        </div>
+    </div>
+  );
+}
+
+export default App;
