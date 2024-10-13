8d069439f6a83917d47e2ac1f170b4033f5e06e5
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-13 19:33:47 +0200

    Add Navbar component and related styles

---
+++ src/frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Data Visualization App</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
