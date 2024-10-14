196ba76505998155460c706fbacb84f071b5eea7
albert-queralto <albert.queralto.lopez@gmail.com>
2024-10-14 19:46:40 +0200

    updated

--- init_config.sh
+++ init_config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash/
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/scheduler/config_init_creator.sh"
+
+exec uvicorn 'backend.app:app' --reload --workers 1 --host 0.0.0.0 --port 8100
 tail -f /dev/null
+
